519b608b40b1ed89a8e0e8e555eed66064a82dee
Aimee Rodríguez <arodriguez.posma@gmail.com>
2022-06-01 16:21:36 -0400

    Creado proyecto base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'event-modal',
+    loadChildren: () => import('./event-modal/event-modal.module').then( m => m.EventModalPageModule)
   }
 ];
 @NgModule({
